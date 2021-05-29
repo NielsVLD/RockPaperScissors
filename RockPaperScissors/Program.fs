9dfe6ba74455e21328b01f9be1c96bd5eece070a
NielsVLD <nielskrommenhoek@hotmail.nl>
2021-05-29 21:54:52 +0200

    Rock paper scissors V2
    V2 of RPS made in F#

--- RockPaperScissors/Program.fs
+++ RockPaperScissors/Program.fs
@@ -1,49 +1,45 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿// Rock Paper Scissors!
 open System
 
 
-
-let RPS() = 
+let RPS3() = 
     
-    let combos = [|"Rock";"Paper";"Scissors"|]
+    let moves = [|"Rock";"Paper";"Scissor"|]
+
+    printf "Lets Play, Rock Paper Scissors!\n\n"
+    printf "Select how many games you want to play : 1, 2 or 3?\n" 
     let x = System.Console.ReadLine()
-    let rnd = System.Random().Next(0,2)
-    //let isInt x = box (int x) :? int
+    let num = System.Int32.Parse(x)
 
-    if(0 <= (int x) && (int x) <= 2) then
-        let computer = combos.[rnd]
-        let player = combos.[(int x)]
+    let rec Game(n : int) =
+        
+        let random = new System.Random()
+        let computer = moves.[random.Next(moves.Length)]
+        
+        printf "Enter: Rock, Paper or Scissor: \n"
+        let player = System.Console.ReadLine()
+        match (player, computer) with
+        | ("Rock", "Paper")
+        | ("Paper", "Scissor")
+        | ("Scissor", "Rock") -> printfn "Computer Wins!"
+        | ("Rock", "Scissor")
+        | ("Paper", "Rock")
+        | ("Scissor", "Paper") -> printfn "Player Wins!"
+        | (player, computer) -> printfn "Its a tie!"
+        
+        printfn "The computer picked %s\n" computer
 
-        if(player = computer) then
-            printf "Its a tie!"
-        if(player = "Rock" && computer = "Paper") then
-            printf "Computer wins!"
-        if(player = "Rock" && computer = "Scissors") then
-            printf "You win!"
-        if(player = "Paper" && computer = "Rock") then
-            printf "You win!"
-        if(player = "Paper" && computer = "Scissors") then
-            printf "Computer wins!"
-        if(player = "Scissors" && computer = "Rock") then
-            printf "Computer wins!"
-        if(player = "Scissors" && computer = "Paper") then
-            printf "You win!"
-        printfn "\nYou picked %s." player
-        printfn "The computer picked %s." computer
-    else
-        printf "\nNumber is not between 0 and 2.\n"
-       
-    
+        match n with
+        | 1 -> "The game has ended. Thanks for playing\n"
+        | n -> Game(n - 1)
+
+    printfn "%s" (Game num)
 
 
 [<EntryPoint>]
 let main argv =
-    printf "Lets Play, Rock Paper Scissors!\n\n"
-    printf "Rules: press a number to pick Rock, Paper or Scissors and see if you can win!\n\n0 = Rock\n1 = Paper\n2 = Scissors\n"
-    printf "Please enter a number (0, 2): "
-    RPS()
 
+    RPS3()
 
 
     0 // return an integer exit code
